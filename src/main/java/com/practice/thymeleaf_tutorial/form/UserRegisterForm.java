5e86c20e5abd20cf1e9273024ed99116d95bcb2e
HJ0216 <6120hj@gmail.com>
2025-08-14 22:12:01 +0900

    ✨Feat: #-- thymeleaf tutorial 완료

---
+++ src/main/java/com/practice/thymeleaf_tutorial/form/UserRegisterForm.java
@@ -0,0 +1,15 @@
+package com.practice.thymeleaf_tutorial.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UserRegisterForm {
+  String name;
+  String email;
+  String password;
+  String gender;
+  String address;
+  String profession;
+
+}
